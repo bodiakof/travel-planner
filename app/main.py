89b460440b6a5b1172ac8a1cc2f308e454252379
Bohdan Khudiakov <b.khudiakov@gmail.com>
2026-03-03 17:50:38 +0200

    feat: add basic http authentication to all endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+import secrets
 
 from app.db import create_db_and_tables
 from app.api.projects import router as projects_router
 from app.api.places import router as places_router
 
+
+security = HTTPBasic()
+
+USERNAME = "admin"
+PASSWORD = "secret"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,9 +24,24 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-app.include_router(projects_router)
-app.include_router(places_router)
+def verify_credentials(
+    credentials: HTTPBasicCredentials = Depends(security),
+):
+    correct_username = secrets.compare_digest(credentials.username, USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, PASSWORD)
+
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+app.include_router(projects_router, dependencies=[Depends(verify_credentials)])
+app.include_router(places_router, dependencies=[Depends(verify_credentials)])
 
 @app.get("/")
 def read_root():
     return {"message": "Travel Planner API is running"}
+
+
