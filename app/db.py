99deb4d1455a48a77bb0fe282084b1c9690dcec1
Bohdan Khudiakov <b.khudiakov@gmail.com>
2026-03-03 15:23:54 +0200

    fix: enable sqlite foreign key enforcement

--- app/db.py
+++ app/db.py
@@ -1,4 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from typing import Generator
 
 
@@ -6,10 +8,15 @@ DATABASE_URL = "sqlite:///./travel.db"
 
 engine = create_engine(DATABASE_URL, echo=False)
 
+@event.listens_for(Engine, "connect")
+def enable_sqlite_fk(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON;")
+    cursor.close()
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
-        
